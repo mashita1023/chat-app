ce29f44a45de2d37aae24ecadf83fb801be7d643
mashita1023 <coloring45@gmail.com>
2020-09-15 11:02:42 +0900

    [fix] change allow function

--- doc/docs.md
+++ doc/docs.md
@@ -1,3 +1,8 @@
+# アロー関数について
+下の読んでわからなかったら教えます。
+[JavaScript アロー関数を説明するよ](https://qiita.com/may88seiji/items/4a49c7c78b55d75d693b)
+
 # 参考
 - [Node.js + Express + Socket.ioで簡易チャットを作ってみる](https://qiita.com/riku-shiru/items/ffba3448f3aff152b6c1)
 - [オブジェクトの移動](https://blog.katsubemakito.net/nodejs/socketio/realtime-chat2)
+- [Webchatアプリを作る](https://www.hiramine.com/programming/chat_nodejs_socketio/01_connect.html)
--- src/server.js
+++ src/server.js
@@ -4,34 +4,40 @@ let http = require('http').Server(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 7000;
 
+let users = {sample:'null',};
+/*  特定のファイルの呼び出し
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
+*/
+// フォルダの指定
+app.use( express.static(__dirname + '/public'));
 
-io.on('connection',function(socket){
-  let name = '';
+//常時
+io.on('connection', (socket) => {
 
   socket.on('message', function(msg){
     console.log('message:' + msg);
     io.emit('message', msg);
-    name = msg.name;
+    users[socket.id] = msg.name;
+    console.log(users);
   });
 
   // メッセージの送信者どうやって特定するん？
   // 名前とidの辞書生成すればいいかな？いいよね？
-  socket.on('disconnect', function(msg) {
+  socket.on('disconnect', (msg) => {
     console.log(msg)
     console.log(socket.id)
-    if (name == '') {
+    if (users[socket.id] == '') {
       let msg = "someone disconnected.";
     } else { 
-      let msg = name + ' disconnected.';
+      let msg = users[socket.id] + ' disconnected.';
     }
     io.emit('logout', msg)
   });
 });
 
-http.listen(PORT, function(){
+http.listen(PORT, () => {
   console.log('server listening. Port:' + PORT);
 });
 
