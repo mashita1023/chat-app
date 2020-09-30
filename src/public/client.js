d0ce42edaf9b0538e0731ee0d3994f53428759b3
mashita1023 <coloring45@gmail.com>
2020-09-30 14:45:03 +0900

    [mod] move object with button

--- src/public/client.js
+++ src/public/client.js
@@ -1,50 +1,9 @@
-class GameEvent {
-    constructor(data) {
-        this.data = data;
-    }
-}
-
-class User {
-    constructor(x, y){
-        this.x = x;
-        this.y = y;
-        this.listeners = {};
-    }
-// 追加するイベント
-    addEventListener(type, listener) {
-        if (!this.listeners[type]) {
-            this.listeners[type] = [];
-        }
-        this.listeners[type].push(listener);
-    }
-// 消すイベント
-    dispatchEvent(type, event) {
-        const listeners = this.listeners[type] || [];
-        listeners.forEach(listener => listener(event));
-    }
-// destroy条件
-    destroyIfContains(x, y) {
-        if (x > this.x && x < this.x + 32 && y > this.y && y < this.y + 32) {
-            this.dispatchEvent("destroy", newGameEvent(this));
-            console.log("destroy")
-        }
-    }
-
-    update(input) {}
-
-    render(context) {
-        context.fillStyle = "rgb(255, 127, 127)";
-        context.fillRect(this.x, this.y, 32, 32);
-    }
-}
 
 let socketio = io();
 let name = '';
-let canvas = document.getElementById("canvas");
-let context = canvas.getContext('2d');
-let users = [];
-
-
+let uid = '';
+let id_value = '';
+let before_id = '';
 
 // 名前入力
 $('#joinForm').submit( () => {
@@ -58,91 +17,53 @@ $('#joinForm').submit( () => {
 });
 
 // chat部分
-$('#message_form').submit( () => {
-    let data = JSON.stringify({ name:name, message:$('#input_msg').val(), action:'chat' });
+$('#messageForm').submit( () => {
+    let data = JSON.stringify({ name:name, message:$('#inputMsg').val() });
     socketio.emit('message', data);
-    $('#input_msg').val('');
+    $('#inputMsg').val('');
     return false;
 });
 
 // 受信
 socketio.on('message', (msg) => {
     let data = JSON.parse(msg);
-    switch(data.action) {
-    case 'mousemove':
-        $('#box-x').text('X: ' + data.x);
-        $('#box-y').text('Y: ' + data.y);
-        $('#mine').css('left', data.x);
-        $('#mine').css('top', data.y);
-        break;
-    case 'chat':
-        $('#messages').append($('<p>').text( data.name + ' : ' + data.message));
-        break;
-    }
+    $('#messages').append($('<p>').text( data.name + ' : ' + data.message));
 });
 
 socketio.on('join', (some) => {
     let data = JSON.parse(some);
     $('#messages').append($('<p>').text(data.msg));
-    document.getElementById('mine').innerHTML = name;
-    //create_circle(data.uid);
-//    create_circle();
+    uid = data.uid;
 })
 
-socketio.on('absense', (data) => {
-    $('#messages').append($('<p>').text(data));
+socketio.on('absense', (msg) => {
+    $('#messages').append($('<p>').text(msg));
 })
 
-canvas.addEventListener("click", e => {
-    const rect = e.target.getBoundingClientRect();
-    const x = e.clientX - rect.left - 1;
-    const y = e.clientY - rect.top - 1;
-    users.forEach(user => user.destroyIFContains(x, y));
+socketio.on('move', (msg) => {
+    let data = JSON.parse(msg);
+
+    if (data.before_id) {
+        let beforeBackgroundImageUrl = document.getElementById(data.before_id).style.backgroundImage;
+
+        if(beforeBackgroundImageUrl === ('url("img/'+data.uid+'.jpg")')) {
+            document.getElementById(data.before_id).style.backgroundImage = "url(img/none.jpg)";
+        }
+        document.getElementById(data.id_value).style.backgroundImage = "url(img/" + data.uid + ".jpg)";
+    } else {
+        document.getElementById(data.id_value).style.backgroundImage = "url(img/" + data.uid + ".jpg)";
+    }
+    //$(data.id_value).style.backgroundImage = "url(img/" + data.uid + ".jpg)";
 })
 
-const FPS = 60;
-const frameTime = 1 / FPS;
-let prevTimestamp = 0;
-let tick = 0;
-
-const update = timestamp => {
-    const elapsed = (timestamp - prevTimestamp) / 1000;
-    if (elapsed <= timestamp) {
-        requestAnimationFrame(update);
-        return;
+function getId(element) {
+    console.log(element.style.backgroundImage)
+    if (element.style.backgroundImage === '' || element.style.backgroundImage === 'url("img/none.jpg")') {
+        before_id = id_value;
+        id_value = element.id;
+    
+        let data = JSON.stringify({ name:name, id_value:id_value, before_id:before_id, uid:uid });
+        socketio.emit('move', data);
     }
 
-    prevTimestamp = timestamp;
-    tick++;
-// 1秒に1回増やす
-    if (tick % FPS === 0) {
-        const x = Math.floor(Math.random() * (320 - 32));
-        const y = Math.floor(Math.random() * (320 - 32));
-        const user = new User(x, y);
-
-        user.addEventListener("destroy", e => {
-            users = users.filter(user => user !== e.data);
-        });
-        users.push(user);
-    }
-    users.forEach(user => user.update());
-
-    context.clearRect(0, 0, 320, 320);
-    users.forEach(user => user.render(context));
-
-    requestAnimationFrame(update);
-};
-
-update();
-
-/*********
- * https://qiita.com/negi/items/6ec0d3cedba499eac81a
-動的に追加した要素はイベントが聞かない
-最初から一つ目は用意しておく
-
-上よりもWebGL使ったほうがよさそう
-
-PixiJSかthree.jsどっちか
-2DだからPixiJSでよさそう！！
-
-*********/
+}
--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 let express = require('express')
 let app = express();
 let http = require('http').Server(app);
+let Canvas = require('canvas');
+let fs = require('fs')
+let dataUriToBuffer = require('data-uri-to-buffer');
+const { create } = require('domain');
+const { publicDecrypt } = require('crypto');
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 7000;
 
@@ -21,9 +26,11 @@ io.on('connection', (socket) => {
   console.log('接続: ' + socket.id)
   socket.on('join', (name_) => {
     nickname = name_;
+    image = create_circle(nickname, socket.id);
+    sendImage = String(image)
     console.log(nickname + ' join.');
     msg = nickname + 'が入室しました。'
-    data = JSON.stringify({ uid:socket.id, msg:msg});
+    data = JSON.stringify({ uid:socket.id, msg:msg, image:sendImage});
     console.log(data)
     io.emit('join', data);
   })
@@ -33,6 +40,11 @@ io.on('connection', (socket) => {
     console.log('message:' + msg);
     io.emit('message', msg);
   });
+
+  socket.on('move', (msg) => {
+    console.log('move:' + msg);
+    io.emit('move', msg);
+  })
 
   // 切断時
   socket.on('disconnect', () => {
@@ -50,3 +62,28 @@ http.listen(PORT, () => {
   console.log('server listening. Port:' + PORT);
 });
 
+//画像生成
+function create_circle (name, id) {
+  let canvas = Canvas.createCanvas(100, 100);
+  let ctx = canvas.getContext('2d');
+
+  ctx.arc(50,50,50,0*Math.PI/180,360*Math.PI/180,false);
+  ctx.fillStyle = 'rgba(0,0,255,0.5)';
+  ctx.fill();
+  ctx.font = '10px Impact';
+  ctx.fillStyle = 'rgba(0,0,0,1.0)';
+  ctx.fillText(name, (100-ctx.measureText(name).width)/2, 50);
+
+  let canvasDataUrl = canvas.toDataURL();
+  let decoded = dataUriToBuffer(canvasDataUrl);
+  save = __dirname + '/public/img/' + id + '.jpg';
+  fs.writeFile(save, decoded, (err)=>{
+    if(err) console.log(`error!::${err}`);
+  });
+
+  return canvas.toDataURL();
+}
+
+/***
+ルーム作ったら閉じたときに画像削除の処理を追加
+***/
